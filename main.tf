79dd5b3441f97037a628eaa19f986e8172dac411
Rohith M <rohith.m068@gmail.com>
2023-07-16 00:23:46 +0530

    Adding Launch template and auto scaling group code

--- main.tf
+++ main.tf
@@ -1,94 +1,90 @@
-##Policy
-#resource "aws_iam_policy" "policy" {
-#  name        = "${var.component}-${var.env}-ssm-pm-policy"
-#  path        = "/"
-#  description = "${var.component}-${var.env}-ssm-pm-policy"
-#
-#  policy = jsonencode(
-#    {
-#      "Version": "2012-10-17",
-#      "Statement": [
-#        {
-#          "Sid": "VisualEditor0",
-#          "Effect": "Allow",
-#          "Action": [
-#            "ssm:GetParameterHistory",
-#            "ssm:GetParametersByPath",
-#            "ssm:GetParameters",
-#            "ssm:GetParameter"
-#          ],
-#          "Resource": "arn:aws:ssm:us-east-1:403215663985:parameter/roboshop.${var.env}.${var.component}.*"
-#        }
-#      ]
-#    })
-#}
-#
-##Role
-#resource "aws_iam_role" "role" {
-#name = "${var.component}-${var.env}-ec2-role"
-#
-#assume_role_policy = jsonencode(
-#{
-#  "Version": "2012-10-17",
-#  "Statement": [
-#    {
-#      "Action": "sts:AssumeRole",
-#      "Principal": {
-#        "Service": "ec2.amazonaws.com"
-#      },
-#      "Effect": "Allow",
-#      "Sid": ""
-#    }
-#  ]
-#})
-#}
-#
-#resource "aws_iam_role_policy_attachment" "policy-attach" {
-#  role      = aws_iam_role.role.name
-#  policy_arn = aws_iam_policy.policy.arn
-#}
-#
-#resource "aws_iam_instance_profile" "instance_profile" {
-#  name =  "${var.component}-${var.env}-ec2-role"
-#  role = "${aws_iam_role.role.name}"
-#}
-#
+
 ##Security Group
-#resource "aws_security_group" "sg" {
-#  name        = "${var.component}-${var.env}-sg"
-#  description = "${var.component}-${var.env}-sg"
-#
-#  ingress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#  }
-#
-#  tags = {
-#    Name = "${var.component}-${var.env}-sg"
-#  }
-#}
-#
-##EC2
-#resource "aws_instance" "instance" {
-#  ami = data.aws_ami.ami.id
-#  instance_type = "t3.small"
-#  vpc_security_group_ids = [aws_security_group.sg.id]
-#  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
-#  tags = merge({
-#    Name = "${var.component}-${var.env}"
-#  },var.tags)
-#}
-#
-#
+resource "aws_security_group" "main" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port        = var.app_port
+    to_port          = var.app_port
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_subnet_cidr
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_ssh_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+resource "aws_launch_template" "main" {
+  name = "${var.component}-${var.env}"
+
+  block_device_mappings {
+    device_name = "/dev/sdf"
+
+    ebs {
+      volume_size = 20
+      encrypted = true
+      kms_key_id = var.kms_key_arn
+    }
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.instance_profile.name
+  }
+  image_id = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = merge({Name = "${var.component}-${var.env}",Monitor = "true"}, var.tags)
+    }
+  }
+  user_data = base64encode("${path.module}/userdata.sh",{
+    env = var.env
+    component = var.component
+  } )
+}
+
+
+
+resource "aws_autoscaling_group" "main" {
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_lb_target_group" "test" {
+  name     = "${var.component}-${var.env}-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+
 ##DNS Record
 #resource "aws_route53_record" "dns" {
 #  zone_id = "Z01893031FJEHFT2WJCRK"
@@ -98,55 +94,5 @@
 #  records = [aws_instance.instance.private_ip]
 #}
 #
-##Null resource for ansible
-#resource "null_resource" "ansible" {
-#  depends_on = [aws_instance.instance, aws_route53_record.dns]
-#  provisioner "remote-exec" {
-#    connection {
-#      type     = "ssh"
-#      user     = "centos"
-#      password = "DevOps321"
-#      host     = aws_instance.instance.public_ip
-#    }
-#
-#    inline = [
-#      "sudo labauto ansible",
-#      "sudo set-hostname -skip-apply ${var.component}",
-#      "ansible-pull -i localhost, -U https://github.com/MROHITH068/roboshop-ansible.git main.yml -e role_name=${var.component} -e env=${var.env}"
-#    ]
-#  }
-#}
 
 
-resource "aws_security_group" "sg" {
-    name        = "${var.component}-${var.env}-sg"
-    description = "${var.component}-${var.env}-sg"
-    vpc_id = var.vpc_id
-    ingress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-    tags = {
-      Name = "${var.component}-${var.env}-sg"
-    }
-  }
-
-resource "aws_instance" "test" {
-  ami = data.aws_ami.ami.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = var.subnet_id
-  tags = {
-    Name = var.component
-  }
-}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,12 @@ variable "component" {}
 variable "tags" {}
 variable "subnet_id" {}
 variable "vpc_id" {}
+variable "kms_key_arn" {}
+variable "allow_ssh_cidr" {}
+variable "app_port" {}
+variable "instance_type" {}
+variable "sg_subnet_cidr" {}
+
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
