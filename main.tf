8e6fa1c265ee6b7e43a16932e6f5f606e20869ed
Rohith M <rohith.m068@gmail.com>
2023-07-16 00:43:51 +0530

    Adding Launch template and auto scaling group code

--- main.tf
+++ main.tf
@@ -69,15 +69,17 @@ resource "aws_autoscaling_group" "main" {
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns = [aws_lb_target_group.main.arn]
+
   launch_template {
     id      = aws_launch_template.main.id
     version = "$Latest"
   }
 }
 
-resource "aws_lb_target_group" "test" {
-  name     = "${var.component}-${var.env}-lb-tg"
-  port     = 80
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}-tg"
+  port     = var.app_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
