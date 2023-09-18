3d057b0f34b12f4960e3e561c67a604e04a75a94
Rohith M <rohith.m068@gmail.com>
2023-09-18 00:58:45 -0500

    Added key into aws_ssm_parameter.

--- iam.tf
+++ iam.tf
@@ -14,9 +14,12 @@ resource "aws_iam_policy" "policy" {
             "ssm:GetParameterHistory",
             "ssm:GetParametersByPath",
             "ssm:GetParameters",
-            "ssm:GetParameter"
+            "ssm:GetParameter",
+            "kms:Decrypt"
           ],
-          "Resource": "arn:aws:ssm:us-east-1:403215663985:parameter/roboshop.${var.env}.${var.component}.*"
+          "Resource": ["arn:aws:ssm:us-east-1:403215663985:parameter/roboshop.${var.env}.${var.component}.*",
+            "arn:aws:ssm:us-east-1:403215663985:parameter/roboshop.${var.env}.docdb.*",
+          var.kms_key_arn]
         }
       ]
     })
