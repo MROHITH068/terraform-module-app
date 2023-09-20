d14347c26d351968b808df366a6add635dcdeff3
Rohith M <rohith.m068@gmail.com>
2023-09-20 02:17:32 -0500

    Chnages and corrections

--- main.tf
+++ main.tf
@@ -47,6 +47,7 @@ resource "aws_launch_template" "main" {
   iam_instance_profile {
     name = aws_iam_instance_profile.instance_profile.name
   }
+
   image_id = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
