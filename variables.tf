3786c9a9bd134deb8249d709f451c73c1b6e3289
Rohith M <rohith.m068@gmail.com>
2023-09-21 12:23:10 -0500

    Added prometheus in app module

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "allow_ssh_cidr" {}
 variable "app_port" {}
 variable "instance_type" {}
 variable "sg_subnet_cidr" {}
-
+variable "allow_prometheus_cidr" {}
 variable "desired_capacity" {}
 variable "max_size" {}
 variable "min_size" {}
